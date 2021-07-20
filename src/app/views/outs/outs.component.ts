481f912ae717d375174b625f4016dd7f1c54022e
Derik Vila <ddcv.eng18@uea.edu.br>
2021-07-19 20:05:36 -0400

    primeiro

---
+++ src/app/views/outs/outs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outs',
+  templateUrl: './outs.component.html',
+  styleUrls: ['./outs.component.css']
+})
+export class OutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
